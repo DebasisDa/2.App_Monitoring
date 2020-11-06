3f179ca3ba3b93133c2be5681a5b1ec844313bc9
Tridibesh Nag <tridibesh.nag@aponiar.com>
2020-11-06 19:57:13 +0530

    Date:06.11.20

--- 1.example-nodejs-app_grafana_prometheous/indexx.js
+++ 1.example-nodejs-app_grafana_prometheous/indexx.js
@@ -1,29 +1,65 @@
-const http = require('http')
-const url = require('url')
-const client = require('prom-client')
+const client = require('prom-client');
+const express = require('express');
+const server = express();
+const apiMetrics = require('prometheus-api-metrics');
+server.use(apiMetrics())
+const register = new client.Registry();
 
-// Create a Registry which registers the metrics
-const register = new client.Registry()
+// Probe every 5th second.
+const intervalCollector = client.collectDefaultMetrics({prefix: 'node_', timeout: 5000, register});
 
-// Add a default label which is added to all metrics
-register.setDefaultLabels({
-  app: 'example-nodejs-app'
+const counter = new client.Counter({
+   name: "node_my_counter",
+   help: "This is my counter"
+});
+
+const gauge = new client.Gauge({
+   name: "node_my_gauge",
+   help: "This is my gauge"
+});
+
+const histogram = new client.Histogram({
+   name: "node_my_histogram",
+   help: "This is my histogram",
+   buckets: [0.1, 5, 15, 50, 100, 500]
+});
+
+const summary = new client.Summary({
+   name: "node_my_summary",
+   help: "This is my summary",
+   percentiles: [0.01, 0.05, 0.5, 0.9, 0.95, 0.99, 0.999]
+});
+
+register.registerMetric(counter);
+register.registerMetric(gauge);
+register.registerMetric(histogram);
+register.registerMetric(summary);
+
+const rand = (low, high) => Math.random() * (high - low) + low;
+
+setInterval(() => {
+   counter.inc(rand(0, 1));
+   gauge.set(rand(0, 15));
+   histogram.observe(rand(0,10));
+   summary.observe(rand(0, 10));
+}, 1000);
+
+server.get('/metrics', (req, res) => {
+   res.set('Content-Type', register.contentType);
+   res.end(register.metrics());
+});
+
+
+
+server.get('/hello', (req, res) => {
+  res.status(200).send({'Message':'Hello World!'})
 })
 
-// Enable the collection of default metrics
-client.collectDefaultMetrics({ register })
+console.log('Server listening to 8080, metrics exposed on /metrics endpoint');
+server.listen(8080);
 
-// Define the HTTP server
-const server = http.createServer(async (req, res) => {
-  // Retrieve route from request object
-  const route = url.parse(req.url).pathname
 
-  if (route === '/metrics') {
-    // Return all metrics the Prometheus exposition format
-    res.setHeader('Content-Type', register.contentType)
-    res.end(register.metrics())
-  }
-})
 
-// Start the HTTP server which exposes the metrics on http://localhost:8080/metrics
-server.listen(8080)
+
+
+
